48406f161f1ccad6a633cc26790e350375cfb138
UNBLOK2020 <unblok2020@gmail.com00>
2021-05-07 10:56:33 +0530

    first

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <div className='header-inner'>
+        <div className='logo'>Givingz</div>
+        <nav>
+          <ul>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
